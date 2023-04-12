24f7c8ab58a839bd759953048bbbe207054c93b9
ren.zhong <ren.zhong@ly.com>
2023-04-12 13:31:21 +0800

    打包跳过测试

--- src/main/java/com/example/hk/demo/api/camera/CameraController.java
+++ src/main/java/com/example/hk/demo/api/camera/CameraController.java
@@ -4,6 +4,7 @@ import com.example.hk.demo.base.ResultDto;
 import com.example.hk.demo.api.camera.req.TakeReq;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CameraController {
 
+    @Autowired
+    CameraService cameraService;
+
     @ApiOperation(value = "拍照")
     @PostMapping(value = "camera/take")
     public ResultDto take(@RequestBody TakeReq request) {
+        cameraService.takePic();
         return ResultDto.success();
     }
 
---
+++ src/main/java/com/example/hk/demo/api/camera/CameraService.java
@@ -0,0 +1,20 @@
+package com.example.hk.demo.api.camera;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @description:
+ * @author: ren.zhong@chebada.com
+ * @date: 2023年04月12日13:23:30
+ **/
+@Service
+public class CameraService {
+
+    /**
+     * 拍照
+     */
+    public void takePic(){
+        //MvCameraControl.
+
+    }
+}
